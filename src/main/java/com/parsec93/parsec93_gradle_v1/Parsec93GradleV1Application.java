82a66745ec45da9e6fa701563bdc99cc6fdb8f30
pasrec93 <rnjs8657@naver.com>
2022-03-04 00:57:12 +0900

    Initial commit

---
+++ src/main/java/com/parsec93/parsec93_gradle_v1/Parsec93GradleV1Application.java
@@ -0,0 +1,13 @@
+package com.parsec93.parsec93_gradle_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parsec93GradleV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parsec93GradleV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/parsec93/parsec93_gradle_v1/Parsec93GradleV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsec93.parsec93_gradle_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parsec93GradleV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
