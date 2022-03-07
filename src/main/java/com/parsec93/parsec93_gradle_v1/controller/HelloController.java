902c543c3a94d81f26a65060f9ac14fe3657148e
pasrec93 <rnjs8657@naver.com>
2022-03-08 01:18:17 +0900

    20220308_MVC & Template & API

--- src/main/java/com/parsec93/parsec93_gradle_v1/controller/HelloController.java
+++ src/main/java/com/parsec93/parsec93_gradle_v1/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.parsec93.parsec93_gradle_v1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,38 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //body 부분에 이 내용을 직접 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; //"hello spring!!!!!!"
+        // 페이지 소스보기를 할 경우 이 return 페이지의 경우 html없이 딱 저 내용만 나옴.
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        //이 방식으로 return 을 하니까 json 형식으로 데이터를 다시 돌려주게 됨.
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
