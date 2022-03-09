64c818c61955f3482f41546844f034a78f182945
pasrec93 <rnjs8657@naver.com>
2022-03-09 12:30:09 +0900

    20220308_MVC & Template & API

--- src/main/java/com/parsec93/parsec93_gradle_v1/controller/HelloController.java
+++ src/main/java/com/parsec93/parsec93_gradle_v1/controller/HelloController.java
@@ -15,14 +15,14 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hello-mvc")
+    @GetMapping("hello-mvc") //ResponseBody가 없으면 템플릿을 사용해서 입력.
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
     }
 
     @GetMapping("hello-string")
-    @ResponseBody //body 부분에 이 내용을 직접 넣어주겠다는 뜻
+    @ResponseBody //body 부분에 이 내용을 그대로 직접 넣어주겠다는 뜻의 어노테이션
     public String helloString(@RequestParam("name") String name) {
         return "hello " + name; //"hello spring!!!!!!"
         // 페이지 소스보기를 할 경우 이 return 페이지의 경우 html없이 딱 저 내용만 나옴.
@@ -38,7 +38,7 @@ public class HelloController {
     }
 
     static class Hello {
-        private String name;
+        private String name;//겟터 셋터 생성 단축키(intelliJ) : ctrl + enter
 
         public String getName() {
             return name;
