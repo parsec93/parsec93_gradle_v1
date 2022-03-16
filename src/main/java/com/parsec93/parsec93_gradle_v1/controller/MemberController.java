1b65ba43db89722e9faa360e66688b7b36040190
pasrec93 <rnjs8657@naver.com>
2022-03-17 00:28:22 +0900

    20220317_회원 웹 기능 - 조회

--- src/main/java/com/parsec93/parsec93_gradle_v1/controller/MemberController.java
+++ src/main/java/com/parsec93/parsec93_gradle_v1/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.parsec93.parsec93_gradle_v1.domain.Member;
 import com.parsec93.parsec93_gradle_v1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +34,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
