b89f3000b78034c71edb889bfb4c30ce059b3f97
pasrec93 <rnjs8657@naver.com>
2022-03-25 01:33:45 +0900

    20220325_순수 JDBC

--- src/main/java/com/parsec93/parsec93_gradle_v1/SpringConfig.java
+++ src/main/java/com/parsec93/parsec93_gradle_v1/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.parsec93.parsec93_gradle_v1;
 
+import com.parsec93.parsec93_gradle_v1.repository.JdbcMemberRepository;
 import com.parsec93.parsec93_gradle_v1.repository.MemberRepository;
 import com.parsec93.parsec93_gradle_v1.repository.MemoryMemberRepository;
 import com.parsec93.parsec93_gradle_v1.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
