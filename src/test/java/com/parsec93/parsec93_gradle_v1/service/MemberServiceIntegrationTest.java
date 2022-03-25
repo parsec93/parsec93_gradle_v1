d707ec80b1d66c0b70973aef447e4980a4ede1f5
pasrec93 <rnjs8657@naver.com>
2022-03-25 12:13:07 +0900

    20220325_스프링 통합 테스트

---
+++ src/test/java/com/parsec93/parsec93_gradle_v1/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,63 @@
+package com.parsec93.parsec93_gradle_v1.service;
+
+import com.parsec93.parsec93_gradle_v1.domain.Member;
+import com.parsec93.parsec93_gradle_v1.repository.MemberRepository;
+import com.parsec93.parsec93_gradle_v1.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepsotiory;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+
+        //when
+        memberService.join(member1);
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
